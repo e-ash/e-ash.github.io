2f49198d39448c9000b424720fa50f45b1a31c97
e-ash <eashh@protonmail.com>
2016-12-22 13:12:55 -0600

    Update apex.js

--- APEX/apex.js
+++ APEX/apex.js
@@ -1,4 +1,10 @@
-const ps1 = "@ ~ $ ";
+let gameStarted = false;
+
+let ps1 = "@ ~ $ ";
+const setPs1 = header => {
+  ps1 = header;
+  cline.innerHTML = ps1;
+};
 
 const input = document.querySelector(".i");
 const log = document.querySelector(".log");
@@ -6,6 +12,25 @@ const cline = document.querySelector(".cl");
 onclick = e => {
   e.preventDefault();
   input.focus();
+};
+
+const processCommand = command => {
+  if (gameStarted) {
+    
+  } else {
+    if (command == "help") {
+      print("ls\n");
+      print("  list information about the files in the current directory.\n");
+      print("run [program]\n");
+      print("  run program specified (e.g. \"run file.sh\").\n");
+    } else if (command == "ls") {
+      print("-rwxr-xr-x 2 gnaiih ring ?<_! !!@ portal.sh\n");
+    } else if (command == "run portal.sh") {
+      gameStarted = true;
+      setPs1("> ");
+      print("hello.");
+    }
+  }
 };
 
 let prev = 0;
@@ -17,7 +42,7 @@ input.onkeydown = e => setTimeout(() => {
   const value = () => (input.value + " ").replace(/ /g, "\u00A0");
   if (e.keyCode == "13") {
     print(ps1 + value() + "\n");
-    
+    processCommand(input.value);
     input.value = "";
   }
   const selStart = input.selectionStart;
@@ -40,10 +65,13 @@ input.onkeydown = e => setTimeout(() => {
   cline.innerHTML = `${ps}${value().substr(0, selStart)}<span class="${classes.join(" ")}">${value().substr(selStart, selLen + 1)}</span>${value().substr(selEnd + 1)}`;
   
   prev = rSelLen;
-})
+});
 const print = msg => {
   log.innerHTML += msg.replace(/\n/g, "<br />");
-}
+};
+const clear = () => {
+  log.innerHTML = "";
+};
 
 print("atkins shell 2\n\nprotip: atkins shell will not harm you\n\n");
 
